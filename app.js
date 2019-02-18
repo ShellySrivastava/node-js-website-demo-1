0194a319c16e19a64f2e426a8c1abb718ef754b9
Shelly Srivastava <shelly.srivastava@softwaysolutions.com>
2019-02-18 08:26:36 +0530

    FEAT: BASIC VIEW SETUP

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const hbs = require('hbs');
 
 const {mongoose} = require('./db/mongoose');
 const product = require('./routes/product.route');
 
 let app = express();
 
+app.set('view engine', 'hbs');
+
 app.get('/', (req, res) => {
-    res.send("server is running");
+    // res.send("server is running");
+    res.render('home.hbs', {title: "Demo Website", body: "Welcome to the party"})
 });
 
 app.use(bodyParser.json());
