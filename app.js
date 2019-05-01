1a344dd966081a571e03e074e4c9979479c78c93
Shelly Srivastava <shelly.srivastava@softwaysolutions.com>
2019-05-01 18:42:03 +0530

    FEAT: BOOTSTRAP THEME INTEGRATION

--- app.js
+++ app.js
@@ -10,15 +10,14 @@ let app = express();
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
-    // res.send("server is running");
     res.render('home.hbs', {title: "Demo Website", body: "Welcome to the party"})
 });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/products', product);
+app.use(express.static(__dirname+ '/public'));
 
-// let port = 8080;
 let port = process.env.PORT || 8080
 app.listen(port, () => {
     console.log(`server listening at port ${port}`);
